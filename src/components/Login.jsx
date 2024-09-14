7019791cfbe5b97c66e5cd07e04e462694481b87
erick <zouozshalom@gmail.com>
2024-09-14 16:36:42 -0500

    Background Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FaMicrosoft } from 'react-icons/fa'; 
+import { FaMicrosoft } from 'react-icons/fa';
+import background from '../assets/background.png';
 
 const Login = () => {
   const handleLogin = () => {
@@ -7,8 +8,14 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="w-full max-w-sm bg-white shadow-md rounded-lg p-6">
+    <div 
+      className="relative flex items-center justify-center h-screen bg-cover bg-center" 
+      style={{ backgroundImage: `url(${background})` }} 
+    >
+      {/* Superposición oscura */}
+      <div className="absolute inset-0 bg-black opacity-30"></div> 
+      
+      <div className="relative w-full max-w-sm bg-white shadow-md rounded-lg p-6 z-10">
         <div className="flex justify-center mb-4">
           <img
             src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiWIWSZx522tAelmtZH81mS1nOZ-_2uwtjr4VLY1qZf7tvuJ08obXkMmA0nCG7PhcLTdz3faWCeBGuxw1expsuXiH0J2i4Kpol7NKQu0NXFACoUuPWJ1w9xG1H751VmMytQ62klNnSCnPM/s400/logo-juegosflorales.png" 
@@ -18,7 +25,7 @@ const Login = () => {
         </div>
         <h1 className="text-2xl font-bold text-center mb-4">Login</h1>
         <p className="text-center mb-4 text-gray-600">
-          Ingresa haciendo uso de tu correo universitario upt.pe
+          Ingresa haciendo uso del correo otorgado por la Universidad.
         </p>
         <button
           className="w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md flex items-center justify-center gap-2 hover:bg-blue-700 transition-colors duration-300"
@@ -28,6 +35,15 @@ const Login = () => {
           Iniciar con Microsoft
         </button>
       </div>
+      
+      <a 
+        href="https://www.upt.edu.pe/upt/web/index.php" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="absolute bottom-4 left-4 text-black underline hover:text-gray-700 z-10"
+      >
+        Universidad Privada de Tacna
+      </a>
     </div>
   );
 };
