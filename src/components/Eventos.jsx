e58996b2a29ce089933ce35be92137249353e077
Erick Mauricio <coartac.erick.mamani@gmail.com>
2024-09-20 11:48:04 -0500

    Se optimizo el background de login

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const facultades = [
-  { nombre: "Todas", abreviatura: "Todas" },
-  { nombre: "Facultad de Ingeniería", abreviatura: "FAING" },
+  { nombre: 'Todas', abreviatura: 'Todas' },
+  { nombre: 'Facultad de Ingeniería', abreviatura: 'FAING' },
   {
-    nombre: "Facultad de Educación, Ciencias de la Comunicación y Humanidades",
-    abreviatura: "FAEDCOH",
+    nombre: 'Facultad de Educación, Ciencias de la Comunicación y Humanidades',
+    abreviatura: 'FAEDCOH',
   },
-  { nombre: "Facultad de Derecho y Ciencias Políticas", abreviatura: "FADE" },
-  { nombre: "Facultad de Ciencias de la Salud", abreviatura: "FACSA" },
-  { nombre: "Facultad de Ciencias Empresariales", abreviatura: "FACEM" },
-  { nombre: "Facultad de Arquitectura y Urbanismo", abreviatura: "FAU" },
+  { nombre: 'Facultad de Derecho y Ciencias Políticas', abreviatura: 'FADE' },
+  { nombre: 'Facultad de Ciencias de la Salud', abreviatura: 'FACSA' },
+  { nombre: 'Facultad de Ciencias Empresariales', abreviatura: 'FACEM' },
+  { nombre: 'Facultad de Arquitectura y Urbanismo', abreviatura: 'FAU' },
 ]
 
 const Eventos = () => {
@@ -19,17 +19,17 @@ const Eventos = () => {
   const [filteredEvents, setFilteredEvents] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
-  const [selectedFacultad, setSelectedFacultad] = useState("Todas")
+  const [selectedFacultad, setSelectedFacultad] = useState('Todas')
   const [showVigentes, setShowVigentes] = useState(false)
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await axios.get("http://localhost:5298/evento")
+        const response = await axios.get('http://localhost:5298/evento')
         setEvents(response.data)
         setFilteredEvents(response.data)
       } catch (err) {
-        setError("No se pudieron cargar los eventos")
+        setError('No se pudieron cargar los eventos')
       } finally {
         setLoading(false)
       }
@@ -44,7 +44,7 @@ const Eventos = () => {
   const filterEvents = () => {
     let filtered = events
 
-    if (selectedFacultad !== "Todas") {
+    if (selectedFacultad !== 'Todas') {
       filtered = filtered.filter((event) => event.facultad === selectedFacultad)
     }
 
@@ -152,19 +152,19 @@ const Eventos = () => {
                   className='bg-white p-8 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out border border-gray-200'
                 >
                   <h3 className='text-2xl font-semibold mb-4 text-purple-800'>
-                    {event.nombre || "Nombre del Evento"}
+                    {event.nombre || 'Nombre del Evento'}
                   </h3>
                   <p className='text-gray-700 mb-2'>
-                    <strong className='text-purple-600'>Fecha Inicio:</strong>{" "}
+                    <strong className='text-purple-600'>Fecha Inicio:</strong>{' '}
                     {new Date(event.fechaInicio).toLocaleDateString()}
                   </p>
                   <p className='text-gray-700 mb-2'>
-                    <strong className='text-purple-600'>Fecha Término:</strong>{" "}
+                    <strong className='text-purple-600'>Fecha Término:</strong>{' '}
                     {new Date(event.fechaTermino).toLocaleDateString()}
                   </p>
                   <p className='text-gray-700 mb-2'>
-                    <strong className='text-purple-600'>Facultad:</strong>{" "}
-                    {event.facultad || "No especificada"}
+                    <strong className='text-purple-600'>Facultad:</strong>{' '}
+                    {event.facultad || 'No especificada'}
                   </p>
                 </div>
               ))}
