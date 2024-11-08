8dd1d06285a27e08b3ae854b550dad89ec958610
Erick Mauricio <coartac.erick.mamani@gmail.com>
2024-11-08 18:14:39 -0500

    Delete version 1

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export { default as CountdownTimer } from './components/CountdownTimer';
-export { default as EventCard } from './components/EventCard';
-export { default as Footer } from './components/Footer';
-export { default as Header } from './components/Header';
-export { default as Navbar } from './components/Navbar';
--- src/main.jsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { JuegosFloralesApp } from './JuegosFloralesApp'
-
-createRoot(document.getElementById('root')).render(<JuegosFloralesApp />)
